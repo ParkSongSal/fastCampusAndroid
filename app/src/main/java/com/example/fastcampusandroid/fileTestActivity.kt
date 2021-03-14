2d6f96a5a0f122dec3b49f88301a9eadaffbcd4a
SangMook Park <tkdanr0311@gmail.com>
2021-03-14 21:16:00 +0900

    Final과제 풀이

---
+++ app/src/main/java/com/example/fastcampusandroid/fileTestActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fastcampusandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class fileTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_file_test)
+    }
+}
