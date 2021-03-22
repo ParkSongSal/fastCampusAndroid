79b5da1d911ccce6997f13b17c5cffd3a2f961a6
SangMook Park <tkdanr0311@gmail.com>
2021-03-22 22:22:34 +0900

    Listener

--- app/src/main/java/com/example/fastcampusandroid/ListenerActivity.kt
+++ app/src/main/java/com/example/fastcampusandroid/ListenerActivity.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_listener.*
 
+var number = 10
 class ListenerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,23 +26,29 @@ class ListenerActivity : AppCompatActivity() {
         // 1) 람다방식
         hello_txt.setOnClickListener{
             Log.d("click", "Hello CLick!")
-
+            hello_txt.text = "안녕하세요"
+            gradient_img.setImageResource(R.drawable.board)
+            number += 10
+            Log.d("number", ""+ number)
         }
 
         // 2) 익명 함수 방식
-        hello_txt.setOnClickListener(object: View.OnClickListener{
-            override fun onClick(v: View?) {
-                Log.d("click", "Hello Click!!")
-            }
-        })
+//        hello_txt.setOnClickListener(object: View.OnClickListener{
+//            override fun onClick(v: View?) {
+//                Log.d("click", "Hello Click!!")
+//            }
+//        })
+//
+//        // 3) 이름이 필요한 경우(click)
+//        val click = object : View.OnClickListener{
+//            override fun onClick(v: View?) {
+//
+//            }
+//        }
+//        hello_txt.setOnClickListener(click)
 
-        // 3) 이름이 필요한 경우(click)
-        val click = object : View.OnClickListener{
-            override fun onClick(v: View?) {
-
-            }
-        }
-        hello_txt.setOnClickListener(click)
+        // 뷰를 조작하는 함수들
+        //hello_txt.text = "안녕하세요"
 
 
 
