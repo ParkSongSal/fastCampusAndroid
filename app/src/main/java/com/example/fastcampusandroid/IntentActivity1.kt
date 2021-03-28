70086e1e11165b3bfa13e473886e3225afdfa639
SangMook Park <tkdanr0311@gmail.com>
2021-03-28 23:07:37 +0900

    #Intent - 암시적 인텐트

--- app/src/main/java/com/example/fastcampusandroid/IntentActivity1.kt
+++ app/src/main/java/com/example/fastcampusandroid/IntentActivity1.kt
@@ -1,6 +1,7 @@
 package com.example.fastcampusandroid
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -13,6 +14,8 @@ class IntentActivity1 : AppCompatActivity() {
 
 
         change_Intent.setOnClickListener{
+
+            // 명시적인텐트
           /*  val intent = Intent(this, IntentActivity2::class.java)
 
             // Dictionary(Key, Value 방식) -> Key와 value를 쌍으로 만들어 저장한다.
@@ -20,13 +23,17 @@ class IntentActivity1 : AppCompatActivity() {
             intent.putExtra("number2", 2)
             startActivity(intent)
         */
-            val intent2 = Intent(this@IntentActivity1, IntentActivity2::class.java)
+            /*val intent2 = Intent(this@IntentActivity1, IntentActivity2::class.java)
             // Apply ->
             intent2.apply{
                 this.putExtra("number1", 1)
                 this.putExtra("number2",2)
             }
-            startActivityForResult(intent2,200)
+            startActivityForResult(intent2,200)*/
+
+            // 암시적 인텐트
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"))
+            startActivity(intent)
         }
     }
 
