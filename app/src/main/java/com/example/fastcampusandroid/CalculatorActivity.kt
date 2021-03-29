adf9b66c9edd519a5689d83a8aee96eb188e1c81
SangMook Park <tkdanr0311@gmail.com>
2021-03-29 22:50:53 +0900

    사칙연산 계산기 - 천단위 comma 표시 - form 식 표시

--- app/src/main/java/com/example/fastcampusandroid/CalculatorActivity.kt
+++ app/src/main/java/com/example/fastcampusandroid/CalculatorActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_calculator.*
+import java.text.DecimalFormat
 
 var new = ""
 var old = ""
@@ -19,72 +20,72 @@ class CalculatorActivity : AppCompatActivity() {
 
         btn1.setOnClickListener{
             new += "1"
-            formTxt.text = new
+            formTxt.text = commaFormat(new)
         }
 
         btn2.setOnClickListener{
             new += "2"
-            formTxt.text = new
+            formTxt.text = commaFormat(new)
         }
 
         btn3.setOnClickListener{
             new += "3"
-            formTxt.text = new
+            formTxt.text = commaFormat(new)
         }
 
         btn4.setOnClickListener{
             new += "4"
-            formTxt.text = new
+            formTxt.text = commaFormat(new)
         }
 
         btn5.setOnClickListener{
             new += "5"
-            formTxt.text = new
+            formTxt.text = commaFormat(new)
         }
 
         btn6.setOnClickListener{
             new += "6"
-            formTxt.text = new
+            formTxt.text = commaFormat(new)
         }
 
         btn7.setOnClickListener{
             new += "7"
-            formTxt.text = new
+            formTxt.text = commaFormat(new)
         }
 
         btn8.setOnClickListener{
             new += "8"
-            formTxt.text = new
+            formTxt.text = commaFormat(new)
         }
 
         btn9.setOnClickListener{
             new += "9"
-            formTxt.text = new
+            formTxt.text = commaFormat(new)
         }
 
         btn0.setOnClickListener{
             new += "0"
-            formTxt.text = new
+            formTxt.text = commaFormat(new)
         }
 
         // 더하기버튼 클릭시
         addBtn.setOnClickListener{
-            calEvent("+", new)
+            calEvent("+", commaFormat(new))
         }
 
         // 빼기버튼 클릭시
         subBtn.setOnClickListener{
-           calEvent("-", new)
+           calEvent("-", commaFormat(new))
         }
 
         // 곱하기버튼 클릭시
         mulBtn.setOnClickListener{
-            calEvent("*", new)
+            calEvent("*", commaFormat(new))
         }
 
         // 나누기 버튼 클릭시
         divBtn.setOnClickListener{
-            calEvent("/", new)
+            calEvent("/", commaFormat(new))
         }
 
         // 초기화버튼 클릭시
@@ -97,7 +98,7 @@ class CalculatorActivity : AppCompatActivity() {
                 Log.d("TAG","new del 전 : $new" )
                 if(new.isNotEmpty()){
                     new = new.substring(0, new.length-1)
-                    formTxt.text = new
+                    formTxt.text = commaFormat(new)
                 }else{
                     Toast.makeText(this,"지울 값이 없습니다.",Toast.LENGTH_SHORT).show()
                     resultTxt.text = ""
@@ -108,8 +109,14 @@ class CalculatorActivity : AppCompatActivity() {
 
         // = 버튼 클릭시
         equalBtn.setOnClickListener{
-            val newSplit = new.split(cal)
-            new = newSplit[1]
+//            val newSplit = new.split(cal)
+//            new = newSplit[1]
+            val oldSplit = old.split(cal)
+            Log.d("TAG", "equal : ${oldSplit.toString()}")
+            old = oldSplit[0].replace(",","")
+
+            Log.d("TAG", "equal old : $old")
+            Log.d("TAG", "equal new : $new")
 
             when (cal){
                 "+"-> {
@@ -125,8 +132,9 @@ class CalculatorActivity : AppCompatActivity() {
                     result = (old.toInt() / new.toInt()).toString()
                 }
             }
+            formTxt.text = commaFormat(old) + cal + commaFormat(new) + "="
             new = result
-            resultTxt.text = result
+            resultTxt.text = commaFormat(result)
         }
     }
 
@@ -149,9 +157,23 @@ class CalculatorActivity : AppCompatActivity() {
         if(lastChar == "-" || lastChar == "+" || lastChar == "*" || lastChar == "/" ){
             new = new.substring(0,new.length-1)
         }
-        old = new
-        new += valueCal
+        //old = new
+        //new += valueCal
+        old = new + valueCal
         cal = valueCal
-        formTxt.text = new
+        Log.d("TAG","calEvent old : $old")
+        new = ""
+        Log.d("TAG","calEvent new : $new")
+        //formTxt.text = new
+        formTxt.text = old
+    }
+
+    // 천단위 콤마 (#,###)
+    private fun commaFormat(value : String) : String{
+        var result = ""
+        val fomatter = DecimalFormat("###,###.##")
+        result = fomatter.format(value.toLong())
+
+        return result
     }
 }
